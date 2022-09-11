1273dc4328520c70d89b7821f714340a33a9e873
ChenxiWong <chinawangchenxi@gmail.com>
2022-09-11 15:35:11 +0800

    初始化执行安装

---
+++ SUMMARY.md
@@ -0,0 +1,23 @@
+# Summary
+
+## 简介
+* [前言](README.md)
+
+## golang笔记
+* [标准库](https://studygolang.com/pkgdoc)
+    * [golangfmt占位符号](blog/golang/fmt_placeholder.md)
+    * [golang自定义日志样式](blog/golang/zap_userdefine.md)
+
+## python笔记
+* [标准库](https://docs.python.org/zh-cn/3/)
+    * [ubuntu环境下C语言拓展cpython](blog/python/ubuntu_clang_python.md)
+* [第三方库](https://wangchenxi.top)
+    * [gunicorn server hook 说明](blog/python/gunicornServerHook.md)
+    * [python Linux ipc通信之消息队列实例](blog/python/pythonLinuxIpcMessageQueue.md)
+    * [python gevent 使用实例](blog/python/pythonGeventDemo.md)
+    * [sysv-ipc 进程间通信的使用](blog/python/python_sysv-ipc_Demo.md)
+
+## 其他
+* [github Page](blog/other/githubPages.md)
+    * [gitbook使用举例](blog/other/gitbookEnvPrepare.md)
+    * [github Actions自动构建](blog/other/GitHubActions.md)
---
+++ book.js
@@ -0,0 +1,89 @@
+module.exports = {
+    "title": "我的文档",
+    "author": "tianv5",
+    "description": "文档",
+    "keywords": "学习,笔记,编程,代码,博客,文章,github",
+    "introduction": {
+        "path": "README.md",
+        "title": "前言",
+    },
+    "variables": {
+        "themeColor": "#3884ff",
+        "themeLou": {
+            // 顶部导航栏配置
+            "nav": [
+                {
+                    "target": "_blank", // 跳转方式: 打开新页面
+                    "url": "https://www.baidu.com",  // 跳转页面
+                    "name": "百度"  // 导航名称
+                }
+            ]
+        }
+    },
+    "plugins": [
+        "highlight",
+        "-lunr",
+        "sharing",
+        "livereload",
+        "chapter-fold",
+        "sitemap",
+        "theme-lou",
+        "back-to-top-button",
+        "code",
+        "prism",
+        "search-pro",
+        "-search"
+    ],
+    "pluginsConfig": {
+        "chapter-fold": {},
+        "theme-lou": {
+            "color": "#5F9EA0", // 主题色
+            "favicon": "static/favicon.ico", // favicon图标
+            "logo": "static/logo.png", // 顶部左侧图标
+            "forbidCopy": false, // 页面是否禁止复制
+            "search-placeholder": "全局搜索", // 搜索框默认文本
+            "book-summary-title": "导航", // 目录标题
+            "book-anchor-title": "本章目录标题", // 本章目录标题
+            "hide-elements": [".summary .gitbook-link"],
+            "imgStyle": {
+                "isCenter": true, // 是否居中(默认为true)
+                "isBox": false, // 是否有边框(默认为true)
+                "otherStyle": 'max-width: 80%;', // 图片其他自定义CSS样式
+            }
+        },
+        "sitemap": {
+            "hostname": "https://tianv5.github.io/docs"
+        },
+        "prism": {
+            "css": [
+                "prismjs/themes/prism-okaidia.css"
+            ],
+            "lang": {
+                "flow": "typescript"
+            }
+        }
+    },
+    "pdf":
+        {
+            "toc":
+                true,
+            "pageNumbers":
+                true,
+            "fontSize":
+                12,
+            "paperSize":
+                "a4",
+            "margin":
+                {
+                    "right":
+                        62,
+                    "left":
+                        62,
+                    "top":
+                        36,
+                    "bottom":
+                        36
+                }
+        }
+}
+;
